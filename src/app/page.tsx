ef38188b6c89a14af2a446e613a6349c8ff15c03
Belat <enochbelawu8@gmail.com>
2025-03-24 15:44:07 +0100

    fixing background tints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
   const issues = filteredProblems(selectedCategory)
   return (
     <div>
-      <header className="bg-[#e6e6e620] h-fit md:h-[1200px] lg:h-[1150px] w-full rounded-lg pt-[122px] md:pt-[172px] 
+      <header className="bg-[#e6e6e620] h-fit md:h-[1200px] lg:h-[1150px] w-full rounded-[12px] md:rounded-[24px] pt-[122px] md:pt-[172px] 
       bg-[url(/header-orange-tint-mobile.svg),_url(/header-grid.svg)] 
       md:bg-[url(/header-orange-tint-tablet.svg),_url(/header-grid.svg)] 
       lg:bg-[url(/header-orange-tint-desktop.svg),_url(/header-grid.svg)] 
@@ -328,9 +328,9 @@ export default function Home() {
         h-150 sm:h-200 md:h-250 lg:h-150 
         bg-no-repeat bg-[length:contain,auto,cover] bg-[position:bottom,top_center,center] 
         md:bg-no-repeat md:bg-[length:contain,auto,cover] md:bg-[position:bottom_center,center_-20%,center] 
-        lg:bg-[length:auto,auto,cover] lg:bg-[position:bottom_right,-70%_center,center]  2xl:bg-[position:bottom_right,150%_center,center]">
-          <div className="flex flex-col gap-6 px-5 md:px-18 lg:w-[60%] mt-20 lg:my-21 max-w-[1360px] mx-auto ">
-            <h2 className={`${inter.className} text-[44px]/[44px] md:text-[73px]/[73px] text-white text-start font-medium`}>
+        lg:bg-[length:auto,auto,cover] lg:bg-[position:bottom_right,-70%_center,center]  2xl:bg-[position:70%_100%,80%_center,center]">
+          <div className="flex flex-col justify-start gap-6 px-5 md:px-18 lg:px-0 lg:w-[60%] lg:ms-20 mt-20 lg:my-21 2xl:max-w-[1360px] 2xl:mx-auto ">
+            <h2 className={`${inter.className} text-[44px]/[44px] md:text-[73px]/[73px] text-white text-start font-medium 2xl:w-2/3`}>
               Still Got Questions? <br />
               Book a Free 15 minutes Consultation.
             </h2>
